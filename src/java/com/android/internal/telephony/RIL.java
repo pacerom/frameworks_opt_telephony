f1b3cbced5b2a244454d788da4c30dc6f06285af
Shareef Ali <shareefalis@cyanogenmod.org>
2013-09-23 18:40:22 +0000

    SamsungQcomRIL: New class and get rid of old and outdated codes
    Consolidate the RIL and base on the CDMA RIL
    Now inherit class related to the Samsung Qualcomm family
    
    Special thanks to Dan Pasanen for coninuing the patcheset
    while my macbook was in repair.
    
    * Rebase on standard ril
    * samsungDriverCall now handled in the superclass
    
    * Supports:
      * Quad-core Exynos4 with Qualcomm modem and later
      * Snapdragon S3 and later
    
    Change-Id: Ie593e32087a9bfeb5d16802a10c2739cf1061676
    
    Conflicts:
    
    	src/java/com/android/internal/telephony/RIL.java
    	src/java/com/android/internal/telephony/SamsungExynos4RIL.java

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -220,6 +220,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
     static final String RILJ_LOG_TAG = "RILJ";
     static final boolean RILJ_LOGD = true;
     static final boolean RILJ_LOGV = false; // STOPSHIP if true
+    protected boolean samsungDriverCall = false;
 
     /**
      * Wake lock timeout should be longer than the longest timeout in
@@ -3076,7 +3077,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
      *
      * @param rilVer is the version of the ril or -1 if disconnected.
      */
-    private void notifyRegistrantsRilConnectionChanged(int rilVer) {
+    protected void notifyRegistrantsRilConnectionChanged(int rilVer) {
         mRilVersion = rilVer;
         if (mRilConnectedRegistrants != null) {
             mRilConnectedRegistrants.notifyRegistrants(
@@ -3296,6 +3297,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
             dc.als = p.readInt();
             voiceSettings = p.readInt();
             dc.isVoice = (0 == voiceSettings) ? false : true;
+            if(samsungDriverCall)
+                 p.readInt();
             dc.isVoicePrivacy = (0 != p.readInt());
             dc.number = p.readString();
             int np = p.readInt();
